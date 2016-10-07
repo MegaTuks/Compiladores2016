361e60adabf8a4e9e6c5ad1faf0c4339c4e3eadd
Ruben Hernandez <alexrum92@gmail.com>
2016-10-06 21:08:11 -0500

    Se agrego Bloque, se arreglo Cont_String, se agrego Identificador a los Tokens. Quite - de nombres de reglas. Arregle identacion, errores de sintaxis, en algunas reglas.

--- scanner.py
+++ scanner.py
@@ -5,7 +5,7 @@ tokens = (
    'OPERADOR_IGUAL', 'OPERADOR_COMPARATIVO', 'EXP_OPERADOR','TERM_OPERADOR', 'OPERADOR_DOSPUNTOS', 'RESI_OPERADOR', 'KEYWORD_PROGRAMA', 
    'KEYWORD_TYPE_ENTERO', 'KEYWORD_TYPE_REAL', 'KEYWORD_TYPE_BOOLEANO', 'KEYWORD_SI', 'KEYWORD_SINO', 'KEYWORD_MIENTRAS', 
    'KEYWORD_CLASE', 'KEYWORD_PRINCIPAL', 'KEYWORD_TYPE_CARACTERES', 'KEYWORD_ENTRADA', 'KEYWORD_SALIDA', 'KEYWORD_FUNCION', 
-   'KEYWORD_CICLO', 'KEYWORD_ENTONCES', 'KEYWORD_NULO', 'KEYWORD_RETORNO',
+   'KEYWORD_CICLO', 'KEYWORD_ENTONCES', 'KEYWORD_NULO', 'KEYWORD_RETORNO', 'IDENTIFICADOR'
 )
 
 reserved = {
@@ -60,14 +60,23 @@ def t_IDENTIFICADOR(t):
         t.type = reserved[t.value]
     return t
 
-def t_ERROR(t):
+def t_CONST_STRING(t): 
+  r'\"[A-Za-z0-9_\(\)\{\}\[\]\<\>\!]*\"'
+  t.value = string(t.value)
+  return t
+
+def t_error(t):
     print("Caracter  Ilegal'%s'" % t.value[0])
     t.lexer.skip(1)
-t_CONST_STRING = r'\"[A-Za-z0-9_\(\)\{\}\[\]\<\>\!]*\"'
+
 
 
 import ply.lex as lex
 lexer = lex.lex()
+
+def p_empty(p):
+    'empty :'
+    pass
 
 def p_Tipo(t):
     '''Tipo : KEYWORD_TYPE_ENTERO
@@ -77,168 +86,185 @@ def p_Tipo(t):
     | IDENTIFICADOR
     '''
 def p_Asignacion(t):
-    ''' Asignacion : IDENTIFICADOR AsignaClass OPERADOR_IGUAL Expression SEMICOLON
-
-
+    ''' 
+    Asignacion : IDENTIFICADOR AsignaClass OPERADOR_IGUAL Expresion SEMICOLON
     '''
 def p_AsignaClass(t):
   '''
-  AsignaClass:  AsignaA
+  AsignaClass :  AsignaA
   | PUNTO IDENTIFICADOR
   | empty
   '''
 def p_AsignaA(t):
     '''
-    AsignaA: CORCHETE_IZQ Expression CORCHETE_DER AsignaB 
+    AsignaA : CORCHETE_IZQ Expresion CORCHETE_DER AsignaB 
     | empty
     '''
 def p_AsignaB(t):
   '''
-  AsignaB: CORCHETE_IZQ Expression CORCHETE_DER
+  AsignaB : CORCHETE_IZQ Expresion CORCHETE_DER
   | empty
   '''
 def p_Funcion(t):
   '''
-  Funcion : KEYWORD_FUNCTION Tipo PARENTESIS_IZQ FuncionA PARENTESIS_DER Bloque
+  Funcion : KEYWORD_FUNCION Tipo PARENTESIS_IZQ FuncionA PARENTESIS_DER Bloque
   '''
 
 def p_FuncionA(t):
   '''
-  FuncionA: Parametro FuncionB
+  FuncionA : Parametro FuncionB
     | empty
   '''
 def p_FuncionB(t):
   '''
-  FuncionB: COMMA FuncionA
+  FuncionB : COMMA FuncionA
     | empty
   '''
 def p_Parametro(t):
   '''
-  Parametro: TIPO IDENTIFICADOR
+  Parametro : Tipo IDENTIFICADOR
   '''
+
+def p_Bloque(t):
+  '''
+  Bloque : BRACKET_IZQ BloqueA BRACKET_DER
+  '''
+
+def p_BloqueA(t):
+  '''
+  BloqueA : Declaracion BloqueA
+  | Asignacion BloqueA
+  | LlamadaFuncion BloqueA
+  | Ciclo BloqueA
+  | Condicion BloqueA
+  | Entrada BloqueA
+  | Salida BloqueA
+  | KEYWORD_RETORNO Valor_Salida BloqueA
+  '''
+
 def p_Clase(t):
   '''
-   Clase: KEYWORD_CLASE IDENTIFICADOR Bloque_Clase
+   Clase : KEYWORD_CLASE IDENTIFICADOR Bloque_Clase
   '''
 def p_Bloque_Clase(t):
   '''
-    Bloque_Clase: BRACKET_IZQ Bloque_ClaseA BRACKET_DER SEMICOLON
+    Bloque_Clase : BRACKET_IZQ Bloque_ClaseA BRACKET_DER SEMICOLON
   '''
 
 def p_Bloque_ClaseA(t):
   '''
-    Bloque_ClaseA: Bloque_ClaseB Bloque_ClaseC
+    Bloque_ClaseA : Bloque_ClaseB Bloque_ClaseC
     | empty
   '''
 def p_Bloque_ClaseB(t):
   '''
-    Bloque_ClaseB: Declaracion_Variable Bloque_ClaseB
+    Bloque_ClaseB : Declaracion Bloque_ClaseB
     | empty
   '''
 def p_Bloque_ClaseC(t):
   '''
-    Bloque_ClaseC: Funcion Bloque_ClaseC
+    Bloque_ClaseC : Funcion Bloque_ClaseC
     | empty
   '''
 def p_Ciclo(t):
   '''
-    Ciclo: KEYWORD_MIENTRAS PARENTESIS_IZQ Expresion PARENTESIS_DER Bloque
+    Ciclo : KEYWORD_MIENTRAS PARENTESIS_IZQ Expresion PARENTESIS_DER Bloque
   '''
 def p_Entrada(t):
   '''
-    Entrada: KEYWORD_ENTRADA IDENTIFICADOR SEMICOLON
+    Entrada : KEYWORD_ENTRADA IDENTIFICADOR SEMICOLON
   '''
 def p_Salida(t):
   '''
-    Salida: KEYWORD_SALIDA IDENTIFICADOR Expresion SEMICOLON
+    Salida : KEYWORD_SALIDA IDENTIFICADOR Expresion SEMICOLON
   '''
 def p_Condicion(t):
   '''
-    Condicion: KEYWORD_SI PARENTESIS_IZQ Expresion PARENTESIS_DER Bloque CondicionA
+    Condicion : KEYWORD_SI PARENTESIS_IZQ Expresion PARENTESIS_DER Bloque CondicionA
   '''
 def p_CondicionA(t):
   '''
-    CondicionA: KEYWORD_SINO Bloque
+    CondicionA : KEYWORD_SINO Bloque
     | empty
   '''
 
 def p_Expresion(t):
   '''
-    Expresion: Exp ExpresionA
+    Expresion : Exp ExpresionA
   '''
 
 def p_ExpresionA(t):
   '''
-    ExpresionA: OPERADOR_COMPARATIVO Exp
+    ExpresionA : OPERADOR_COMPARATIVO Exp
     | empty
   '''
 
 def p_Exp(t):
   '''
-    Exp: Termino ExpA
+    Exp : Termino ExpA
   '''
 
 def p_ExpA(t):
   '''
-    ExpA: EXP_OPERADOR Exp
+    ExpA : EXP_OPERADOR Exp
     | empty
   '''
 
 def p_Termino(t):
   '''
-    Termino: Factor TerminoA
+    Termino : Factor TerminoA
     | empty
   '''
 
 def p_TerminoA(t):
   '''
-    TerminoA: TERM_OPERADOR Termino
+    TerminoA : TERM_OPERADOR Termino
     | empty
   '''
 
 def p_Factor(t):
   '''
-    Factor: Valor_Salida
-    | PARENTESIS_IZQ Exp PARENTESIS_Der
+    Factor : Valor_Salida
+    | PARENTESIS_IZQ Exp PARENTESIS_DER
   '''
 
-def p_Llamada-Funcion(p):
+def p_LlamadaFuncion(p):
   '''
-    Llamada-Funcion: IDENTIFICADOR PARENTESIS_IZQ Llamada-FuncionA PARENTESIS_Der
-  '''
-
-def p_Llamada-FuncionA(p):
-  '''
-    Llamada-FuncionA: Expresion Llamada-FuncionB
+    LlamadaFuncion : IDENTIFICADOR PARENTESIS_IZQ LlamadaFuncionA PARENTESIS_DER
   '''
 
-def p_Llamada-FuncionB(p):
+def p_LlamadaFuncionA(p):
   '''
-    Llamada-FuncionB: COMMA Llamada-FuncionA
+    LlamadaFuncionA : Expresion LlamadaFuncionB
+  '''
+
+def p_LlamadaFuncionB(p):
+  '''
+    LlamadaFuncionB : COMMA LlamadaFuncionA
     | empty
   '''
 
 def p_Declaracion(t):
    '''
-   Declaracion: Parametro AsignaA SEMICOLON
+   Declaracion : Parametro AsignaA SEMICOLON
    '''
 
 def p_Programa(t):
   '''
-    Programa:  ProgramaA Funcion-Principal
+    Programa :  ProgramaA FuncionPrincipal
   '''
 
 def p_ProgramaA(t):
   '''
-    ProgramaA: Declaracion ProgramaA
+    ProgramaA : Declaracion ProgramaA
     | Funcion ProgramaA
     | Clase ProgramaA
     | empty
   '''
 
-def p_Funcion-Principal(t):
+def p_FuncionPrincipal(t):
   '''
-    Funcion-Principal: KEYWORD_FUNCION KEYWORD_PRINCIPAL PARENTESIS_IZQ PARENTESIS_DER Bloque
+  FuncionPrincipal : KEYWORD_FUNCION KEYWORD_PRINCIPAL PARENTESIS_IZQ PARENTESIS_DER Bloque
   '''
   
 import ply.yacc as yacc
